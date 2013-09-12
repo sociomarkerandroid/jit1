e5a940b7358e738010d0dfda7c7eff048c3862b6
sociomarkerandroid <sociomarker.android@gmail.com>
2013-09-12 21:56:20 +0530

    Revert "fgvfg"
    This reverts commit fb772c9da34b40a8d3209affe8b39c8ccad94194.

--- androidwebservice.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once "admin/lib/sm-constant.php";
-include_once "admin/lib/sm-connection.php";
-include_once "admin/lib/pf.php";
-$uu = aGetAllProductsByCat('Poncho');
-echo json_encode($uu);
-?>
